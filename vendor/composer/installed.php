e6514bde07e1342f2215acc424f2517d694bbbd4
SaamMi <13660518+SaamMi@users.noreply.github.com>
2026-03-22 00:15:53 +0000

    chore: PHP Pint styling

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'saammi/any-chat',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ee2f6c3c69657d8a0bf98aa880827b52d2c3f442',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -1077,9 +1077,9 @@
             ),
         ),
         'saammi/any-chat' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ee2f6c3c69657d8a0bf98aa880827b52d2c3f442',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
